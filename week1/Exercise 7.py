ad9e5d0387323be511a486ab891d79e1e49b1df4
Semakon <Semakon@gmail.com>
2017-02-07 09:43:32 +0100

    improved Exercise 7

--- week1/Exercise 7.py
+++ week1/Exercise 7.py
@@ -1,12 +1,16 @@
 def divisors(a):
     a = abs(a)
-    if a == 1:
+    n = a
+    if n == 1:
         return {1}
-    res = {1, a}
+    res = {1, n}
     i = 2
-    while i < a:
+    while i < n:
         if a % i == 0:
             res.add(i)
+            temp = a // i
+            res.add(temp)
+            n = temp
         i += 1
     return sorted(res)
 
@@ -34,12 +38,16 @@ def primefact(a):
                 else:
                     res[i] = 1
                 n = n // i
+        if n <= 1:
+            break
     return res
 
 
 # bullet point 1
 print(divisors(147))
+print(divisors(13195))
 
 
 # bullet point 2
 print(primefact(147))
+print(primefact(13195))
